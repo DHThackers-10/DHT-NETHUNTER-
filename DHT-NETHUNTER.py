2f1611288a5f9fe0b070e9dbce139e3e3ac08f7d
DHT-HACKERS <htdark6@gmail.com>
2025-05-23 15:38:13 +0500

    Update DHT-NETHUNTER.py

--- DHT-NETHUNTER.py
+++ DHT-NETHUNTER.py
@@ -206,8 +206,7 @@ def final_instructions():
     table.add_row("nethunter kex &", "Start GUI")
     table.add_row("nethunter kex stop", "Stop GUI")
     console.print(table)
-    console.print(Panel.fit"[green]This tool is for ethical hacking education only.\n"     
-    ))
+    console.print(Panel.fit("[green]echo "nameserver 8.8.8.8" > /etc/resolv.conf[/green]"))
 def move_chroot_to_home(chroot):
     home = os.environ["HOME"]
     target = os.path.join(home, chroot)
